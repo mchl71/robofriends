380f510133dcedd4ed17e45662c0d88a790b432e
Michel Böker <michel-boeker@gmx.de>
2020-03-25 22:09:20 +0100

    Updates

---
+++ precache-manifest.3389498d99ec399b742757b646b55ff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021d3bc7e4fbcbdc09a33c2b18a49c90",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "dfca63819fe197b97101",
+    "url": "/robofriends/static/css/main.542785ad.chunk.css"
+  },
+  {
+    "revision": "541edbd8124ffd22746a",
+    "url": "/robofriends/static/js/2.bf9239df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.bf9239df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfca63819fe197b97101",
+    "url": "/robofriends/static/js/main.231a7eb6.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
